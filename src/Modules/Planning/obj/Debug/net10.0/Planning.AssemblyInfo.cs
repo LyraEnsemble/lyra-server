e161634ab0a1064b2a796b6cf6deedc0d6805b9f
TaielsinH <taielstuder@gmail.com>
2026-03-02 17:14:44 -0300

    integracion de migraciones

--- src/Modules/Planning/obj/Debug/net10.0/Planning.AssemblyInfo.cs
+++ src/Modules/Planning/obj/Debug/net10.0/Planning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Planning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a583c69699a4185815d1afbcae8d3fd09e984a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Planning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Planning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
